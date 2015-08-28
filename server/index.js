0bf68ef65c48f642e45e30859067028dada2a885
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-08-28 17:37:30 +0100

    Changed CORS config

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multipart());
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
