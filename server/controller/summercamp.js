94850cd2bf4ab1f78e837f9989c0b00ef89ebca6
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-08-28 17:31:27 +0100

    Added route for summercamp mail sender

---
+++ server/controller/summercamp.js
@@ -0,0 +1,29 @@
+var mandrill = require('node-mandrill')('2a0fZX4MNBT6jCSitJZilw'),
+    jade = require('jade'),
+    urlencode = require('urlencode');
+
+exports.send = function (req, res) {
+    var data = req.body;
+
+    if (!data.email) {
+        return res.status(400).send('Email required');
+    }
+
+    var emailObj = {
+        from_email  : data.user_email,
+        from_name   : data.name,
+        to          : [ { email: data.email } ],
+        subject     : data.name + ' has sent you their latest creation, "' + data.title + '"',
+        text        : 'foobar',
+        html        : jade.renderFile('views/template.jade', data)
+    };
+
+    mandrill('/messages/send', { message: emailObj }, function (err) {
+
+        if (err) {
+            return res.status(403).send(err.message);
+        }
+
+        res.json({ response: 'Message has been sent to the recipient' });
+    });
+};
