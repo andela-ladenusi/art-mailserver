6c537b4568d981d432669ca6fd950715e68d0050
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-11-23 12:50:46 +0100

    Changed routes

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,6 @@ var router = express.Router();
 router.get('/card/:itemId', require('./controller/card').view);
 router.get('/card/:itemId/edit', require('./controller/card').edit);
 router.post('/card/:itemId/send', require('./controller/card').send);
-router.post('/summercamp/send', require('./controller/summercamp').send);
+router.post('/send', require('./controller/emailer').send);
 
 module.exports = router;
